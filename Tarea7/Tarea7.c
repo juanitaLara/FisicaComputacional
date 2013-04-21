5bf8787ad28894ccbe7a65c784a2bb23e73e96c0
Juanita Lara <jm.lara576@uniandes.edu.co>
2013-04-20 20:42:55 -0500

    Segunda versión

--- Tarea7/Tarea7.c
+++ Tarea7/Tarea7.c
@@ -5,19 +5,20 @@
 #define M 1.0E31
 #define m 1.0E26
 #define gamma 5.0/3.0
-#define g 9.8
+#define g 6.67E-11
 #define c 7.9512E55
 
 
-float funcion_1(float y1);
+float funcion_1(float y1, float y2);
 float funcion_2(float y1, float y2);
 float *runge_kutta_slope(float h, float t, float y1, float y2);
 
 int main(void){
 
-  float h = 1E3;
+  float h = 1.0E3;
   int i;
   int n_points;
+  float *p;
   float *t;
   float *y1;
   float *y2;
@@ -27,9 +28,10 @@ int main(void){
 
   n_points=(int)((1.5E6+h)/h);
 
-  t= malloc(n_points * sizeof(float));
-  y1= malloc(n_points * sizeof(float));
-  y2= malloc(n_points * sizeof(float));
+  t = malloc(n_points * sizeof(float));
+  y1 = malloc(n_points * sizeof(float));
+  y2 = malloc(n_points * sizeof(float));
+  p = malloc(n_points * sizeof(float));
 
   if(!t){
     printf("There is something wrong with t\n");
@@ -47,10 +49,16 @@ int main(void){
     exit(1);
   }
 
+if(!p){
+    printf("There is something wrong with p\n");
+    exit(1);
+  }
+
  /*Condiciones iniciales*/
  t[0]=0.0;
  y1[0]=1.7E10;
  y2[0]=0.0;
+ p[0]=5.6E5;
  slopey1=0.0;
  slopey2=0.0;
 
@@ -69,8 +77,9 @@ int main(void){
    t[i+1] = t[i] + h;
    y1[i+1] = y1[i] + h*slopey1;
    y2[i+1] = y2[i] + h*slopey2;
+   p[i+1] = c*pow(y1[i],-5);
 
-   fprintf(fileout,"%f %f %f\n", t[i], y1[i], y2[i]);
+   fprintf(fileout,"%f %f %f %f\n", t[i], y1[i], y2[i], p[i]);
    
 
  }
@@ -117,7 +126,7 @@ float *runge_kutta_slope(float h, float t, float y1, float y2){
  
 
  
-  k1_1 = funcion_1(y1);
+  k1_1 = funcion_1(y1,y2);
   k1_2 = funcion_2(y1,y2);
 
   /*First step*/
@@ -125,7 +134,7 @@ float *runge_kutta_slope(float h, float t, float y1, float y2){
   y1_1 = y1 + (h/2.0)*k1_1;
   y1_2 = y2 + (h/2.0)*k1_2;
 
-  k2_1 = funcion_1(y1_1);
+  k2_1 = funcion_1(y1_1,y1_2);
   k2_2 = funcion_2(y1_1,y1_2);
 
   /*Second step*/
@@ -133,7 +142,7 @@ float *runge_kutta_slope(float h, float t, float y1, float y2){
   y2_1 = y1 + (h/2.0)*k2_1;
   y2_2 = y2 + (h/2.0)*k2_2;
 
-  k3_1 = funcion_1(y2_1);
+  k3_1 = funcion_1(y2_1,y2_2);
   k3_2 = funcion_2(y2_1,y2_2);
 
   /*Third step*/
@@ -141,7 +150,7 @@ float *runge_kutta_slope(float h, float t, float y1, float y2){
   y3_1 = y1 + (h)*k3_1;
   y3_2 = y2 + (h)*k3_2;
 
-  k4_1 = funcion_1(y3_1);
+  k4_1 = funcion_1(y3_1,y3_2);
   k4_2 = funcion_2(y3_1,y3_2);
 
   /*Fourth step*/
@@ -164,10 +173,10 @@ float *runge_kutta_slope(float h, float t, float y1, float y2){
 
 
 
-float funcion_1(float y1){
+float funcion_1(float y1,float y2){
 
 
-  return y1;
+  return y2;
 
 }
 
